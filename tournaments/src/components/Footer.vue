220d70080dc581de953962a8839a45c5d2831999
Sooraj M <61335345+SOORAJ-SJ@users.noreply.github.com>
2023-03-06 05:49:25 +0000

    fix: UX improvement for footer

--- tournaments/src/components/Footer.vue
+++ tournaments/src/components/Footer.vue
@@ -1,7 +1,18 @@
 <script setup>
+    import { schedules } from '../stores/schedules.store'
+    import { scores } from '../stores/scores.store'
+    import { storeToRefs } from 'pinia'
+    import { STATUSES } from '../globals'
+
+    const schedulesStore = schedules()
+    const scoresStore = scores()
+    const { getSheduleStatus } = storeToRefs(schedulesStore)
+    const { getStatus } = storeToRefs(scoresStore)
 </script>
 <template>
-    <footer class="text-bg-dark mt-4">
+    <footer 
+    class="text-bg-dark mt-4"
+    v-if="getStatus !== STATUSES.PENDING || getSheduleStatus !== STATUSES.PENDING">
         <div class="p-4">©Team Sattva</div>
     </footer>
 </template>
